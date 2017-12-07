9bf9df85d007a8e4864922d2f6b62c6367b63bb4
Hanson Rynsburger <hanson.rynsburger@gmail.com>
2017-12-07 09:50:38 +0100

    remove unnecessary codes

--- src/app.js
+++ src/app.js
@@ -16,12 +16,6 @@ class App extends Component {
     };
   }
 
-  getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
   neighbors(i,j) {
     var result = [];
     for (var offset_1 = -1; offset_1 <= 1; offset_1++) {
